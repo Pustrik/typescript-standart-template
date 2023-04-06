98da72ca30acb283515289808351bda2313e5882
Dmytro Pustovalov <dmytro.pustovalov@itomy.ch>
2023-04-06 14:57:10 +0300

    init template

---
+++ .dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+async function bootstrap() {
+    console.log(`Simple typescript template`);
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
