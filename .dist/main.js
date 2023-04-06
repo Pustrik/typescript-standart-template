9f7c56125e6b85165b89bb54b0ba28acdb98ecf7
Dmytro Pustovalov <126162223+PustovalovDmytro@users.noreply.github.com>
2023-04-06 15:03:16 +0300

    Delete .dist directory

--- .dist/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-async function bootstrap() {
-    console.log(`Simple typescript template`);
-}
-bootstrap();
-//# sourceMappingURL=main.js.map
